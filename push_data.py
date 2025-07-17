6e02240cd9d51f6f954c79f08945fd88443efcc0
Gourav Desettty <gourav.desetty@gmail.com>
2025-07-17 21:03:03 +0530

    ETL pipeline

--- push_data.py
+++ push_data.py
@@ -1,15 +1,62 @@
+import os
+import sys
+import json
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+load_dotenv()
 
-uri = "mongodb+srv://Gourav:Gourav123@cluster0.p5ihobp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+import certifi
+ca = certifi.where()
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import CustomException
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract:
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def cv_to_json_converter(self, file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+
+            return records
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def insert_data_mongodb(self, records, database, collection):
+        try:
+            self.database = database
+            self.records = records
+            self.collection = collection
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL, tlsCAFile=ca)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return (len(self.records))
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+if __name__ == "__main__":
+    FILE_PATH = "Network_Data\phisingData.csv"
+    DATABASE = "Gourav"
+    COLLECTION = "NetworkData"
+    network_obj = NetworkDataExtract()
+    records = network_obj.cv_to_json_converter(FILE_PATH)
+    print(records)
+    no_of_records = network_obj.insert_data_mongodb(records, DATABASE, COLLECTION)
+    print(no_of_records)
