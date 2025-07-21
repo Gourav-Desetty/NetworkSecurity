ff59d5ecdaf51293d988645aa42700ce1dabc90a
Gourav Desettty <gourav.desetty@gmail.com>
2025-07-21 23:06:41 +0530

    Added a return value in data transformation and Completed Model Trainer

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import CustomException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig,ModelTrainerConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
     try:
@@ -28,6 +29,13 @@ if __name__ == "__main__":
         data_transformation = DataTransformation(data_transformation_config, data_validation_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info("Data Transformation completed")
+
+        logging.info("Model Training Started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training Artifact Created")
 
     except Exception as e:
         raise CustomException(e, sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -6,6 +6,8 @@ import os, sys
 import numpy as np
 import dill
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def read_yaml_file(file_path)->dict:
     try:
@@ -42,3 +44,49 @@ def save_obj(file_path: str, obj: object):
             pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys) from e
+
+
+def load_obj(file_path:str) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+def load_numpy_array_data(file_path:str) -> np.array:
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            params = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score    
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
